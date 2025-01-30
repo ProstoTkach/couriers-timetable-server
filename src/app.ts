c69dd28b2ae2ff7155c1c5368c3030a13a893c92
ProstoTkach <90304795+ProstoTkach@users.noreply.github.com>
2025-01-31 01:39:18 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173", // Укажи домен фронтенда
+    origin: process.env.CLIENT_URL, 
     methods: ["GET", "POST","PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   };
